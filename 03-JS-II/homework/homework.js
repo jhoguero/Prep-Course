0177de5c1ed09ca7cbbe534283ba94d0715220ea
JhonatanG <jhoguero21@gmail.com>
2022-10-19 09:55:28 -0500

    ultima actualizacion del taller

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -127,12 +127,12 @@ function colors(color) {
     // Si "numero" es divisible entre 5, devuelve "buzz"
     // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
     // De lo contrario, devuelve el numero
-    if (numero % 3 === 0){
-      return "fizz";
+    if (numero % 3 === 0 && numero % 5 === 0){
+      return "fizzbuzz";
     }else if (numero % 5 === 0){
       return "buzz";
-    }else if (numero % 3 === 0 && numero % 5 === 0){
-      return "fizzbuzz";
+    }else if (numero % 3 === 0){
+      return "fizz";
     }else {
       return numero;
     }
@@ -145,7 +145,17 @@ function colors(color) {
     //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
     //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
     //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-
+    if (num1 < 0 || num2 < 0 || num3 < 0){
+      return  "Hay negativos";
+    }else if (num1 === 0 || num2 === 0 || num3 === 0){
+      return "Error"
+    }else if (num1 > num2 && num1 > num3 && num1 > 0){
+      return "Número 1 es mayor y positivo"
+    }else if (num3 > num1 && num3 > num2){
+      return (num3 + 1);
+    }else {
+      return false
+    }
   }
 
   function esPrimo(numero) {
